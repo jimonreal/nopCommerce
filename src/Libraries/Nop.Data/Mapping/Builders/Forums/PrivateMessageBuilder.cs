d1dbbc0c2e56ad5a9fdb62618b4ef4694de0055b
José Monreal Bailey <jmonrealbailey@salesforce.com>
2026-02-27 20:25:40 +0000

    Fixing Ambiguity of PirvateMessages

--- src/Libraries/Nop.Data/Mapping/Builders/Forums/PrivateMessageBuilder.cs
+++ src/Libraries/Nop.Data/Mapping/Builders/Forums/PrivateMessageBuilder.cs
@@ -1,15 +1,16 @@
-﻿using System.Data;
+using System.Data;
 using FluentMigrator.Builders.Create.Table;
 using Nop.Core.Domain.Customers;
 using Nop.Core.Domain.Forums;
 using Nop.Data.Extensions;
+using ForumPrivateMessage = Nop.Core.Domain.Forums.PrivateMessage;
 
 namespace Nop.Data.Mapping.Builders.Forums;
 
 /// <summary>
 /// Represents a private message entity builder
 /// </summary>
-public partial class PrivateMessageBuilder : NopEntityBuilder<PrivateMessage>
+public partial class PrivateMessageBuilder : NopEntityBuilder<ForumPrivateMessage>
 {
     #region Methods
 
@@ -20,10 +21,10 @@ public partial class PrivateMessageBuilder : NopEntityBuilder<PrivateMessage>
     public override void MapEntity(CreateTableExpressionBuilder table)
     {
         table
-            .WithColumn(nameof(PrivateMessage.Subject)).AsString(450).NotNullable()
-            .WithColumn(nameof(PrivateMessage.Text)).AsString(int.MaxValue).NotNullable()
-            .WithColumn(nameof(PrivateMessage.FromCustomerId)).AsInt32().ForeignKey<Customer>().OnDelete(Rule.None)
-            .WithColumn(nameof(PrivateMessage.ToCustomerId)).AsInt32().ForeignKey<Customer>().OnDelete(Rule.None);
+            .WithColumn(nameof(ForumPrivateMessage.Subject)).AsString(450).NotNullable()
+            .WithColumn(nameof(ForumPrivateMessage.Text)).AsString(int.MaxValue).NotNullable()
+            .WithColumn(nameof(ForumPrivateMessage.FromCustomerId)).AsInt32().ForeignKey<Customer>().OnDelete(Rule.None)
+            .WithColumn(nameof(ForumPrivateMessage.ToCustomerId)).AsInt32().ForeignKey<Customer>().OnDelete(Rule.None);
     }
 
     #endregion
